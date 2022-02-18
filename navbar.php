a7e53a974e6e14ba715ce405fed43718d15b4383
Andrew DePace <93351057+Hyperion1232@users.noreply.github.com>
2022-02-18 10:26:29 -0500

    navbar.php

---
+++ navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+  2 <html>
+  3 <head>
+  4         <style>
+  5                 body {
+  6                         margin: 0px;
+  7                 }
+  8                 ul {
+  9                         list-style-type: none;
+ 10                         margin: 0px;
+ 11                         padding: 0px;
+ 12                         font-weight: 900;
+ 13                 }
+ 14 
+ 15                 .topnav {
+ 16                         position: -webkit-sticky;
+ 17                         position: sticky;
+ 18                         top: 0;
+ 19                         background-color: #EFEFEF;
+ 20                         overflow: hidden;
+ 21                 }
+ 22         </style>
+ 23 </head>
+ 24 
+ 25 <body>
+ 26         <div class = "topnav">
+ 27                 <ul>
+ 28 
+ 29                 </ul>
+ 30         </div>
+ 31 </body>
+ 32 </html>
