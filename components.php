29805aa119d47eab604c89d02357e06f02e74791
Andrew DePace <93351057+Hyperion1232@users.noreply.github.com>
2022-02-21 16:33:50 -0500

    Create components.php

---
+++ components.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<head>
+<style>
+
+.smallbutton{
+height: 45px;
+width: 84px;
+font-size: 14px;
+border-radius: 6px;
+}
+
+.smallbutton:hover{
+background-color: rgba(0, 117, 201, 0.6);
+color: #FFFFFF;
+}
+
+.smallbutton:active{
+background-color: #00528C;
+color: #FFFFFF;
+}
+
+.smallbutton:disabled{
+background-color: #EFEFEF;
+color: #7A7A7A;
+}
+
+.mediumbutton{
+height: 52px;
+width: 123px;
+font-size: 16px;
+border-radius: 6px;
+}
+
+.mediumbutton:hover{
+background-color: rgba(0, 117, 201, 0.6);
+color: #FFFFFF;
+}
+
+.mediumbutton:active{
+background-color: #00528C;
+color: #FFFFFF;
+}
+
+.mediumbutton:disabled{
+background-color: #EFEFEF;
+color: #7A7A7A;
+}
+
+.largebutton{
+height: 59px;
+width: 142px;
+font-size: 18px;
+border-radius: 6px;
+}
+
+.largebutton:hover{
+background-color: rgba(0, 117, 201, 0.6);
+color: #FFFFFF;
+}
+
+.largebutton:active{
+background-color: #00528C;
+color: #FFFFFF;
+}
+
+.largebutton:disabled{
+background-color: #EFEFEF;
+color: #7A7A7A;
+}
+
+.bluewithwhitetext{
+background-color: #0075C9;
+color: #FFFFFF
+}
+
+.whitewithbluetext{
+background-color: #FFFFFF;
+color: #0075C9;
+border: 1px solid #0075C9;
+}
+
+.graywithblacktext{
+background-color: #F2F2F5;
+color: #3A3A3C;
+}
+
+</style>
+</head>
