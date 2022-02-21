28ba15ce68464bac29ad4b906d93032012f2cbdd
Cole Nussear <80608048+Nusse0712@users.noreply.github.com>
2022-02-21 18:01:45 -0500

    Added the template files and files for navbar

--- navbar.php
+++ navbar.php
@@ -1,32 +1,18 @@
-<!DOCTYPE html>
-  2 <html>
-  3 <head>
-  4         <style>
-  5                 body {
-  6                         margin: 0px;
-  7                 }
-  8                 ul {
-  9                         list-style-type: none;
- 10                         margin: 0px;
- 11                         padding: 0px;
- 12                         font-weight: 900;
- 13                 }
- 14 
- 15                 .topnav {
- 16                         position: -webkit-sticky;
- 17                         position: sticky;
- 18                         top: 0;
- 19                         background-color: #EFEFEF;
- 20                         overflow: hidden;
- 21                 }
- 22         </style>
- 23 </head>
- 24 
- 25 <body>
- 26         <div class = "topnav">
- 27                 <ul>
- 28 
- 29                 </ul>
- 30         </div>
- 31 </body>
- 32 </html>
+<html> 
+<head>
+	<link href="navbarFormat.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head> 
+<body>
+<header>
+	<nav>
+		<ul>
+	<li><a href="https://dcwater.com/" id="image"><img src="dcWaterLogo.png"></a></li>
+	<li><a href="#"><img src="searchIcon.png"></a></li>
+	<li><a href="#"><img src="menuIcon.png"></a></li>
+	    </ul>
+
+	</nav>
+</header>
+</body>
+</html> 
